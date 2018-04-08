f26bc875dc85c786b36aabd90fff55c540c6d9e3
ROBOTECH-ESP <38182611+ROBOTECH-ESP@users.noreply.github.com>
2018-04-08 12:39:37 +0000

    Add files via upload

---
+++ new_pov/new_pov.ino
@@ -0,0 +1,33 @@
+/*Title: POV_Display prototype sketch
+  Author: Alioune Woula Ndiaye
+  Company : ROBOTECH-ESP
+  Date:31/03/2018
+  Version:1.1
+  Description: Main program for the POV Display*/
+
+#include "new_pov_lib.h"
+
+SoftwareSerial bluetooth (BLUETOOTH_RX, BLUETOOTH_TX);
+String TXT = "TEST";
+
+
+void setup() {
+  for (int i = 0; i < CHAR_HEIGHT; i++)
+    pinMode(LEDpins[i], OUTPUT);
+
+  pinMode(BLUE_DISPLAY, OUTPUT);
+  pinMode(RED_DISPLAY, OUTPUT);
+
+  pinMode(HALL_SENSOR, INPUT);
+  //Serial.begin(9600);
+  //bluetooth.begin(9600);
+  digitalWrite(BLUE_DISPLAY, HIGH);
+  //digitalWrite(RED_DISPLAY, HIGH);
+}
+
+void loop() {
+  scrollingDisplayLeftBis();
+  delay(1000);
+}
+
+
---
+++ new_pov/new_pov_lib.h
@@ -0,0 +1,28 @@
+#include <Arduino.h>
+
+
+#include <SoftwareSerial.h>
+
+/*Macros*/ 
+#define PERSISTANCE_PERIOD 500 
+#define TIMES_TO_REPEAT 3
+#define CHAR_HEIGHT 8
+#define CHAR_WIDTH 5
+#define MIN_ASCII 32
+#define MAX_ASCII 126
+#define BLUE_DISPLAY 12
+#define RED_DISPLAY A0
+#define BLUETOOTH_TX 3
+#define BLUETOOTH_RX 2
+#define HALL_SENSOR 13
+
+const int LEDpins[] = {11, 10, 9, 8, 7, 6, 5, 4}; //MSB->LSB
+
+/*Prototypes*/
+void displayFromCol (int column);
+void printCharCol (char character , int column);
+void scrollingDisplayLeft ();
+void displayFromColBis (int column);
+void scrollingDisplayLeftBis ();
+
+
